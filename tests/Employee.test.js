252650e14d44dc6e31878e14f765ee135b48b445
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-01-14 16:54:12 -0400

    Add testing

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,3 @@
-//const {test, expect} = require('@jest/globals');
 const Employee = require('../employee_lib/Employee');
 
 // describe("EmployeeParentClass", () => {
@@ -12,9 +11,29 @@ const Employee = require('../employee_lib/Employee');
 //     })
 // })
 
+const newEmployee = new Employee(100, "Joe", "joe@employee.com");
+
 test("Creates a new Employee object", () =>{
-    const newEmployee = new Employee(100, "Joe", "joe@employee.com");
+    // const newEmployee = new Employee(100, "Joe", "joe@employee.com");
     expect(newEmployee.id).toEqual(100);
     expect(newEmployee.name).toBe("Joe");
     expect(newEmployee.email).toBe("joe@employee.com") 
+})
+
+test("returns the id of the employee", () =>{
+    // const newEmployee = new Employee(100, "Joe", "joe@employee.com");
+    expect(newEmployee.getId()).toEqual(100);
+
+})
+
+test("returns the name of the employee", () =>{
+    // const newEmployee = new Employee(100, "Joe", "joe@employee.com");
+    expect(newEmployee.getName()).toBe("Joe");
+
+})
+
+test("returns the name of the email", () =>{
+    // const newEmployee = new Employee(100, "Joe", "joe@employee.com");
+    expect(newEmployee.getEmail()).toBe("joe@employee.com");
+
 })
