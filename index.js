4d42e3f64f1f01f500835a51083fcc0bc4da2374
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-01-15 18:17:58 -0400

    Adding helper templates -- Only manager functionality

--- index.js
+++ index.js
@@ -6,83 +6,97 @@ const Employee = require('./employee_lib/Employee');
 const Engineer = require('./employee_lib/Engineer');
 const Intern = require('./employee_lib/Intern');
 const Manager = require('./employee_lib/Manager');
+const generateTeam = require('./src/template')
 
 //Array to store team member objects
 const teamMember = [];
 
 //Array to ask "Parent/ Common questions"
-const questions = 
-[
-    {       
-      type: "input",
-      message: "Employee ID:  ",
-      name: "id",
-    },
 
-    {       
+const questions = async () => {
+    const userInput = await inquirer.prompt
+    ([
+        {       
         type: "input",
-        message: "Employee Name:  ",
-        name: "name",
-    },
+        message: "Employee ID:  ",
+        name: "id",
+        },
+        {       
+            type: "input",
+            message: "Employee Name:  ",
+            name: "name",
+        },
+        
+        {       
+            type: "input",
+            message: "Email Address:  ",
+            name: "email",
+        }, 
+        
+        {       
+            type: "list",
+            message: "Employee Role:  ",
+            choices: ["Manager", "Engineer", "Intern", "None"],
+            name: "role",
+        },
+    ])
+
     
-    {       
-        type: "input",
-        message: "Email Address:  ",
-        name: "email",
-    }, 
-    
-    {       
-        type: "list",
-        message: "Employee Role:  ",
-        choices: ["Manager", "Engineer", "Intern", "None"],
-        name: "role",
-    },
+};
 
-];
+// } 
 
-const managerQuestions = [
-    {
-        type: "input",
-        message: "Enter office number:  ",
-        name: "officeNumber"
+// const managerQuestions = [
+//     {
+//         type: "input",
+//         message: "Enter office number:  ",
+//         name: "officeNumber"
 
-    }
-]
+//     }
+// ]
 
-//Function to run app
-function init(){
-    inquirer.prompt(questions)
-    .then(function(data){
-        console.log(data.role);
-        if (data.role == "Manager"){
-            // inquirer.prompt(managerQuestions)
-            // .then(function(resp){
-            //     console.log(resp.officeNumber);
-            // })
-            createManager(data);
+// //Function to run app
+// function init(){
+//     questions();
+//     //inquirer.prompt(questions)
+//     .then(function(data){
+//         console.log(data.role);
+//         if (data.role == "Manager"){
+//             // inquirer.prompt(managerQuestions)
+//             // .then(function(resp){
+//             //     console.log(resp.officeNumber);
+//             // })
+//             createManager(data);           
                                 
-            }
+//             }
         
-        } )
+//         } )
         
-        // create function to call questions for ea employee role 
-        // create Team array and upend objects into team array
-        // Use team array to create HTML - I think!!!!
-    }
+//         // create function to call questions for ea employee role 
+//         // create Team array and upend objects into team array
+//         // Use team array to create HTML - I think!!!!
+        
+//     }
             
 
-function createManager(data){
-    //let officeNum = inquirer.prompt(managerQuestions)
-    //let newManager = new Manager(data.id, data.name, data.email, data.role, officeNum);      
-    //console.log(officeNumber)
-    inquirer.prompt(managerQuestions)
-    .then(function(resp){        
-        let newManager = new Manager(data.id, data.name, data.email, data.role, resp.officeNumber); 
-        //console.log(newManager);
-        teamMember.push(newManager);
-        //console.log(teamMember);
-    })   
-}
+// function createManager(data){
+//     //let officeNum = inquirer.prompt(managerQuestions)
+//     //let newManager = new Manager(data.id, data.name, data.email, data.role, officeNum);      
+//     //console.log(officeNumber)
+//     inquirer.prompt(managerQuestions)
+//     .then(function(resp){        
+//         let newManager = new Manager(data.id, data.name, data.email, data.role, resp.officeNumber); 
+//         //console.log(newManager);
+//         teamMember.push(newManager);
+//         //console.log(teamMember);
+//         return teamMember;
+//     })   
+// }
 
-// calling the app
-init();
+// async function makeTeam(){
+//     fs.writeFile('./dist/index.html', generateTeam(teamMember));
+// }
+
+// // calling the app
+// init();
+// //makeTeam();
